6bd19325f2f0da6cdbb371a63fe119598644e444
Mohammed Mustafa <mohammed112025@gmail.com>
2023-10-02 04:02:21 -0700

    Auto pushing

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * Description: check_cycle - checks if there is a cycle in the linkedlist
+ * Input:
+ *	@list: (pointer to node): points to the first node of the linked list
+ * Return:
+ *	- 0 if there is no cycle
+ *	- 1 if there is a cycle
+ */
+int check_cycle(listint_t *list)
+{
+	listint_t *current = list;
+
+	if (list == NULL)
+		return (0);
+
+	while (current)
+	{
+		if (current->next == list)
+			return (1);
+		current = current->next;
+	}
+	return (0);
+}
---
+++ 0x00-python-hello_world/free_listint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * free_listint - frees a listint_t list
+ * @head: pointer to list to be freed
+ * Return: void
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *current;
+
+	while (head != NULL)
+	{
+		current = head;
+		head = head->next;
+		free(current);
+	}
+}
