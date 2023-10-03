5d6c09d6faa66e2a6c0736aa787744c92123770f
Mohammed Mustafa <mohammed112025@gmail.com>
2023-10-03 04:08:11 -0700

    Auto pushing

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,7 +2,7 @@
 import random
 number = random.randint(-10000, 10000)
 ldigit = abs(number) % 10
-const_str = f"Last digit of {number:d} is {ldigit:d} and is " 
+const_str = f"Last digit of {number:d} is {ldigit:d} and is "
 if ldigit > 5:
     print(const_str + "greater than 5")
 elif ldigit == 0:
