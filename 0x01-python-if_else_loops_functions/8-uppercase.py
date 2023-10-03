6d533f3995508e3a6ed6499da26bf2f62513a2a1
Mohammed Mustafa <mohammed112025@gmail.com>
2023-10-03 05:04:34 -0700

    Auto pushing

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -3,7 +3,6 @@ def uppercase(str):
     for char in str:
         asc_code = ord(char)
         if 97 <= asc_code < 123:
-            print("{:s}".format(chr(asc_code - 32)), end="")
-        else:
-            print(char, end="")
+            char = chr(asc_code - 32)
+        print(char, end="")
     print()
