5e46bee1761f3740e913281474fd36d7169e80af
Mohammed Mustafa <mohammed112025@gmail.com>
2023-10-05 03:45:25 -0700

    Auto pushing

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -2,10 +2,7 @@
 if __name__ == "__main__":
     import hidden_4
     hid_mods = dir(hidden_4)
-    unbuilt_in = []
     for mod in hid_mods:
         if mod[:2] == "__":
             continue
-        unbuilt_in.append(mod)
-    unbuilt_in.sort()
-    print(*unbuilt_in, sep='\n')
+        print(mod)
