d1f8aff5765f12fbfa58967fdd03015e7565afb5
Mohammed Mustafa <mohammed112025@gmail.com>
2023-10-05 04:39:42 -0700

    Auto pushing

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 def magic_calculation(a, b):
-    add = magic_calculation_102.add
-    sub = magic_calculation_102.sub
-
+    from magic_calculation_102 import add, sub
     if a < b:
         c = add(a, b)
         for i in range(4, 6):
