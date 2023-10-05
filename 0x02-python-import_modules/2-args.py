a9ab2e941dd6e7558e0a759e33dac938a7401cb8
Mohammed Mustafa <mohammed112025@gmail.com>
2023-10-05 02:55:07 -0700

    Auto pushing

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,9 +5,9 @@ if __name__ == "__main__":
     if (n_args == 1):
         print("0 argument.")
     elif (n_args == 2):
-        print("1 arugment:")
+        print("1 argument:")
         print(f"1: {argv[1]}")
     else:
-        print(f"{n_args - 1} arguments")
+        print(f"{n_args - 1} arguments:")
         for i in range(1, n_args):
             print(f"{i}: {argv[i]}")
