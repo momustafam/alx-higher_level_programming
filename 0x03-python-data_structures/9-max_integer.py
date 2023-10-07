00a2fbac9b286212907ae59aebc5e0ed92606c73
Mohammed Mustafa <mohammed112025@gmail.com>
2023-10-06 22:47:27 -0700

    Auto pushing

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -7,7 +7,7 @@ def max_integer(my_list=[]):
 
     Returns: the max element in my_list
     '''
-    if my_list = []:
+    if my_list == []:
         return None
     _max = my_list[0]
     for elem in my_list:
