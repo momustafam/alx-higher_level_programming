ba0d993c255c35983c9a4f9e20d72ec3e1026e8b
Mohammed Mustafa <mohammed112025@gmail.com>
2023-10-06 21:47:04 -0700

    Auto pushing

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -7,7 +7,7 @@ def print_reversed_list_integer(my_list=[]):
 
     Returns: NULL
     '''
-    if len(my_list) == 0:
+    if len(my_list) is None:
         print("", end="")
     else:
         for elem in my_list[::-1]:
