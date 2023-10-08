70496708718dfd1aea4c59e42b033b7b8aad6b79
Mohammed Mustafa <mohammed112025@gmail.com>
2023-10-08 02:57:32 -0700

    Auto pushing

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -4,11 +4,12 @@
 
 void print_python_list_info(PyObject *p)
 {
-	long int i, len = PyList_Size(p);
-	PyListObject *object = (PyListObject *)p;
+	long int size = PyList_Size(p);
+	int i;
+	PyListObject *obj = (PyListObject *)p;
 
-	printf("[*] Size of the Python List = %li\n", len);
-	printf("[*] Allocated = %li\n", object->allocated);
-	for (i = 0; i < len; i++)
-		printf("Element %i: %s\n", i, Py_TYPE(object->ob_item[i])->tp_name);
+	printf("[*] Size of the Python List = %li\n", size);
+	printf("[*] Allocated = %li\n", obj->allocated);
+	for (i = 0; i < size; i++)
+		printf("Element %i: %s\n", i, Py_TYPE(obj->ob_item[i])->tp_name);
 }
