97ba45b0e0e09d02cb3ce43a1d19093f95b9b04a
Mohammed Mustafa <mohammed112025@gmail.com>
2023-10-08 00:27:09 -0700

    Auto pushing

---
+++ 0x03-python-data_structures/11-delete_at.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def delete_at(my_list=[], idx=0):
+    ''' deletes the item at a specific position in a list
+
+    Args:
+        my_list (list): the list being searched
+        idx (int): index of the item
+
+    Returns: return the updated list
+    '''
+    if idx < 0 or idx >= len(my_list):
+        return my_list
+
+    del my_list[idx]
+    return my_list
