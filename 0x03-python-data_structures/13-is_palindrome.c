4a0e57852c8187a3eb1d1dfecacea331cf5fa59a
Mohammed Mustafa <mohammed112025@gmail.com>
2023-10-08 22:26:21 -0700

    Auto pushing

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,72 +1,84 @@
 #include "lists.h"
 
-/**
- * reverse_listint - reverses a linked list
- * @head: pointer to the first node in the list
- *
- * Return: pointer to the first node in the new list
- */
-void reverse_listint(listint_t **head)
-{
-	listint_t *prev = NULL;
-	listint_t *current = *head;
-	listint_t *next = NULL;
 
-	while (current)
-	{
-		next = current->next;
-		current->next = prev;
-		prev = current;
-		current = next;
-	}
+listint_t *reverse_list(listint_t *);
 
-	*head = prev;
-}
 
 /**
- * is_palindrome - checks if a linked list is a palindrome
- * @head: double pointer to the linked list
+ * Description: is_palindrome - checks if a singly linked list is a palindrome
  *
- * Return: 1 if it is, 0 if not
+ * Args:
+ *	@head: (pointer): head of the linked list being checked
+ * Return: 0 if it's not a plaindrome, 1 otherwise
  */
 int is_palindrome(listint_t **head)
 {
-	listint_t *slow = *head, *fast = *head, *temp = *head, *dup = NULL;
+	listint_t *q1 = *head, *q2 = *head, *temp = *head;
+	int len = 0, i = 1, q1_end, q2_start;
 
-	if (*head == NULL || (*head)->next == NULL)
+	/* get the length of the linked list */
+	while (temp)
+	{
+		len++;
+		temp = temp->next;
+	}
+	
+	/* handle the linked list if it is empty or with one node */
+	if (len == 1 || len == 0)
 		return (1);
 
+	/* store the index of end of first half and start of the second one */
+	q1_end = len / 2;
+	q2_start = (len % 2 == 0) ? (len / 2 + 1) : (len / 2 + 2);
+
+	/* split the linked list to two sublists q1, and q2 */
 	while (1)
 	{
-		fast = fast->next->next;
-		if (!fast)
+		if (i = q1_end)
 		{
-			dup = slow->next;
+			temp = q2;
+			q2 = q2->next;
+			temp->next = NULL;
+			q2 = (++i == q2_start) ? q2 : q2->next;
+			q2 = reverse_list(q2);
 			break;
 		}
-		if (!fast->next)
-		{
-			dup = slow->next->next;
-			break;
-		}
-		slow = slow->next;
+		i++;
+		q2 = q2->next;
 	}
 
-	reverse_listint(&dup);
-
-	while (dup && temp)
+	/* comapre each element in q1 with the corresponding one in q2 */
+	while (q2)
 	{
-		if (temp->n == dup->n)
+		if (q2->n == q1->n)
 		{
-			dup = dup->next;
-			temp = temp->next;
+			q2 = q2->next;
+			q1 = q1->next;
 		}
 		else
 			return (0);
 	}
+	return (1);
+}
 
-	if (!dup)
-		return (1);
 
-	return (0);
+/**
+ * Description: reverse_list - reverses a linke list
+ *
+ * Args:
+ *	head: (pointer): the head of the linked list
+ *
+ * Return: a pointer to the new head of the reversed linke list
+ */
+listint_t *reverse_list(listint_t *head)
+{
+	listint_t *temp, *prev = NULL;
+	while (head)
+	{
+		temp = head;
+		head = head->next;
+		temp->next = prev;
+		prev = temp;
+	}
+	return (temp);
 }
