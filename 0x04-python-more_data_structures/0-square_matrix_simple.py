16300aeaf0489dbe9df9c15a51f51e759b4cf5a5
Mohammed Mustafa <mohammed112025@gmail.com>
2023-10-11 07:26:38 -0700

    Auto pushing

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    ''' computes the square value of all integers of a matrix
+
+    Parameters:
+    -----------
+    matrix: 2 dimensional array
+    the container being squared
+    
+    Returns:
+    --------
+    new_matrix: 2 dimensional array
+    a container that includes squared values of the given matrix
+    '''
+    new_matrix = [list(map(lambda x: x**2, arr)) for arr in matrix]
+    return new_matrix
+
