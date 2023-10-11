868272a6bc474c199d54584ff2794a6531a2480a
Mohammed Mustafa <mohammed112025@gmail.com>
2023-10-11 09:16:26 -0700

    Auto pushing

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
+    if type(roman_string) != 'str' or romand_string is None:
+        return 0
     roman_nums = {
             'I': 1,
             'V': 5,
