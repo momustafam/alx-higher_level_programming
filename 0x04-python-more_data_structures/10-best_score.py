47195f99e0d294e465d7d18edd9a177eb16b9627
Mohammed Mustafa <mohammed112025@gmail.com>
2023-10-11 10:10:20 -0700

    Auto pushing

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -4,8 +4,7 @@ def best_score(a_dictionary):
     if a_dictionary is None:
         return None
 
-    max_int = max(a_dictionary.values())
+    max_int = max(list(a_dictionary.values()))
     for key in a_dictionary:
         if a_dictionary[key] == max_int:
             return key
-    return None
