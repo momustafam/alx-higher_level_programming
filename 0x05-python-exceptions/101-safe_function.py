4c6e70ebaeb90577cd8df438c46dad3fe9f97842
Mohammed Mustafa <mohammed112025@gmail.com>
2023-10-23 11:12:41 -0700

    Auto pushing

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -6,5 +6,5 @@ def safe_function(fct, *args):
     try:
         return fct(*args)
     except Exception as err:
-        print(f"Exception: {err}")
+        print(f"Exception: {err}", file=stderr)
         return None
