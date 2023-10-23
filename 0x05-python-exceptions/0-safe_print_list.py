cecaa80775e0c9ae7cc74470fc7093a536a0eddd
Mohammed Mustafa <mohammed112025@gmail.com>
2023-10-23 07:40:58 -0700

    Auto pushing

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
+    n = 0
     try:
         for i in range(x):
             print(my_list[i], end="")
-        print()
-    except:
-        i -= 1
-        print()
+            n += 1
+    except IndexError:
+        pass
     finally:
-        return i + 1
+        print()
+        return n
