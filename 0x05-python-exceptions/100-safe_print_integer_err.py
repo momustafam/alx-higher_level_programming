11a5ff5a8bdf24b20061233096288856bc471c21
Mohammed Mustafa <mohammed112025@gmail.com>
2023-10-23 11:04:20 -0700

    Auto pushing

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+from sys import stderr
+def safe_print_integer_err(value):
+    try:
+        print("{:d}".format(value))
+        return True
+    except ValueError as err:
+        print(f"Exception: {err}", file=stderr)
+        return False
