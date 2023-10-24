70f32f9e72db4c1061294f71715b4c75c8764e33
Mohammed Mustafa <mohammed112025@gmail.com>
2023-10-24 16:01:31 -0700

    Auto pushing

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -33,7 +33,8 @@ class Square:
         """Update the value of the position attribute."""
         if (not isinstance(value, tuple) or
                 len(value) != 2 or
-                not all(isinstance(num, int) for num in value)):
+                not all(isinstance(num, int) for num in value)
+                not all(num >= 0 for num in value)):
             raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = value
 
