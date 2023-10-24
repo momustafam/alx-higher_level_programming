7d3557d9c089a772067e8cef816f71662937ee87
Mohammed Mustafa <mohammed112025@gmail.com>
2023-10-24 14:55:09 -0700

    Auto pushing

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Square module"""
+
+
+class Square:
+    """An incomplete class"""
+    def __init__(self, size):
+        """Instantiate a Square class with size equal to given size"""
+        self.__size = size
