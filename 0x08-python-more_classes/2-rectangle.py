1fe02be7409f2af420056684dceb97a033e544e6
Mohammed Mustafa <mohammed112025@gmail.com>
2023-10-30 06:03:45 -0700

    Auto pushing

---
+++ 0x08-python-more_classes/2-rectangle.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+"""
+    The ``rectangle`` module
+
+    A very simple module, since it have a one class called ``Rectangle``
+    that defines a rectangle
+"""
+
+
+class Rectangle:
+    '''
+        A class that defines a rectangle
+    '''
+
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+    def area(self):
+        return self.width * self.height
+
+    def perimeter(self):
+        return (self.width + self.height) * 2
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
