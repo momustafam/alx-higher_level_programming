69bf54bab0e1de40de7cb8e575556bab18ec578c
Mohammed Mustafa <mohammed112025@gmail.com>
2023-10-30 06:04:49 -0700

    Auto pushing

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -20,6 +20,8 @@ class Rectangle:
         return self.width * self.height
 
     def perimeter(self):
+        if self.width == 0 or self.height == 0:
+            return 0
         return (self.width + self.height) * 2
 
     @property
