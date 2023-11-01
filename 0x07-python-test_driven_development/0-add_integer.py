6912582559c962183e57bf1a3ccf88e3d25efa60
Mohammed Mustafa <mohammed112025@gmail.com>
2023-10-31 22:46:41 -0700

    Auto pushing

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -11,12 +11,12 @@ def add_integer(a, b=98):
         Return addition of 2 integers
     """
 
-    if not (isinstance(a, int) or isinstance(a, float) or b is float("inf"):
+    if not (isinstance(a, int) or isinstance(a, float) or b is float("NaN"):
         raise TypeError("a must be an integer")
     elif isinstance(a, float):
         a = int(a)
 
-    if not (isinstance(b, int) or isinstance(b, float) or b is float("inf"):
+    if not (isinstance(b, int) or isinstance(b, float) or b is float("NaN"):
         raise TypeError("b must be an integer")
     elif isinstance(b, float):
         b = int(b)
