a713162edeb51673ab368754de2b161243ffc0b7
Mohammed Mustafa <mohammed112025@gmail.com>
2023-10-31 23:28:39 -0700

    Auto pushing

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -7,6 +7,7 @@
 
 
 def say_my_name(first_name, last_name=""):
+    """Print the user's name."""
     if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
     if not isinstance(last_name, str):
