35decc13c57a3b50f045da0c453d61c85aa4a186
Mohammed Mustafa <mohammed112025@gmail.com>
2023-11-06 12:14:17 -0800

    Auto pushing

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 """Easy Task"""
 
+
 def add_attribute(obj, name, value):
     '''add a new attribute to an object if it's possible'''
+
     if not hasattr(obj, '__dict__') and not hasattr(obj, '__slots__'):
         raise TypeError("can't add new attribute")
     elif not hasattr(obj, name) and hasattr(obj, '__slots_'):
