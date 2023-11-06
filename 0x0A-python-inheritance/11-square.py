1864c5b858cbf295803cf7dd1cc67cb6fb32b77d
Mohammed Mustafa <mohammed112025@gmail.com>
2023-11-06 11:41:54 -0800

    Auto pushing

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -27,7 +27,7 @@ class Square(Rectangle):
         self.__size = size
 
     def __str__(self):
-        return f"[Square] {size}/{size}"
+        return f"[Square] {self.__size}/{self.__size}"
 
     def area(self):
         """Computes the area of a Square instance
