7b24e683c67939bb294c480695c41aba4cda5497
Mohammed Mustafa <mohammed112025@gmail.com>
2023-11-06 10:47:22 -0800

    Auto pushing

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -10,4 +10,4 @@ def inherits_from(obj, a_class):
         from the specidied class, otherwise return False
     '''
 
-    return issubclass(obj, a_class)
+    return issubclass(type(obj), a_class) and type(obj) is not a_class
