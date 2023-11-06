fc0550ffb2631545ca61381bc094a2e465507bc7
Mohammed Mustafa <mohammed112025@gmail.com>
2023-11-06 10:40:42 -0800

    Auto pushing

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+    The is_kind_of_class module
+"""
+
+
+def is_kind_of_class(obj, a_class):
+    '''Return True if the obj is an instance of a_class'''
+
+    return isinstance(obj, a_class)
