34ae8a598897a7389505ce065d02db800a140e79
Mohammed Mustafa <mohammed112025@gmail.com>
2023-11-07 13:40:50 -0800

    Auto pushing

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+'''
+    The ``1-write_file`` module
+'''
+
+
+def write_file(filename="", text=""):
+    '''
+    Writes a string to a text file(UTF8) 
+
+    Return the number of characters
+    '''
+
+    with open(filename, 'w', encoding='UTF-8') as f:
+        f.write(text)
