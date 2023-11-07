9a3b517e2ab922a5684f79167afa7b0485f43130
Mohammed Mustafa <mohammed112025@gmail.com>
2023-11-07 14:07:35 -0800

    Auto pushing

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -8,5 +8,5 @@ import json
 def load_from_json(filename):
     '''Creates an object from a JSON file.'''
 
-    with open(filename, encoding='utf-8') as f:
+    with open(filename, 'r', encoding='utf-8') as f:
         return json.load(f)
