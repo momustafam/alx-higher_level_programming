90c4db6bd241e52810e9515aa4d8d087924ed9f1
Mohammed Mustafa <mohammed112025@gmail.com>
2023-11-07 13:54:48 -0800

    Auto pushing

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+'''
+    The 3-to_json_string module
+'''
+import json
+
+
+def to_json_string(my_obj):
+    '''Return the JSON representation of a string'''
+
+    return json.dumps(my_obj)
