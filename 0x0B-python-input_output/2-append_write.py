0ea5c2a629d3478bd512525b4331e30d70524e91
Mohammed Mustafa <mohammed112025@gmail.com>
2023-11-07 13:50:10 -0800

    Auto pushing

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+'''
+    The 2-append_write module.
+'''
+
+
+def append_write(filename="", text=""):
+    '''
+        Append a string at the end of a file.
+
+        Return the number of characters added
+    '''
+
+    with open(filename, 'a', encoding='utf-8') as f:
+        return f.append(text)
