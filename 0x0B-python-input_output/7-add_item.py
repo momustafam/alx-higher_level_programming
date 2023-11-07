e33cbdc7a4260f95a229448f2ccf7d9e44150d34
Mohammed Mustafa <mohammed112025@gmail.com>
2023-11-07 14:34:15 -0800

    Auto pushing

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+'''The 7-add_item module'''
+import json
+from sys import argv
+
+save = __import__("5-save_to_json_file").save_to_json_file
+load = __import__("6-load_from_json_file").load_from_json_file
+
+def add_item():
+    f_name = "add_item.json"
+    args = list(sys.argv[1:])
+
+    try:
+        data = load_from_json_file("add_item.json")
+    except Exception:
+        data = []
+
+    data.extend(args)
+    save_to_json_file(data, "add_item.json")
