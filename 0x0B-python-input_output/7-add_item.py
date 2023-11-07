8cfd6d2e663dfcd3f3b2022221e360306f3ccc96
Mohammed Mustafa <mohammed112025@gmail.com>
2023-11-07 14:35:14 -0800

    Auto pushing

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -11,11 +11,11 @@ def add_item():
     args = list(sys.argv[1:])
 
     try:
-        data = load_from_json_file("add_item.json")
+        data = load("add_item.json")
     except Exception:
         data = []
 
     data.extend(args)
-    save_to_json_file(data, "add_item.json")
+    save(data, "add_item.json")
 
 add_item()
