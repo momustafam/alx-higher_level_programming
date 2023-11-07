75fd289434444b074843721b87ab94d20928df48
Mohammed Mustafa <mohammed112025@gmail.com>
2023-11-07 13:36:40 -0800

    Auto pushing

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -8,4 +8,4 @@ def read_file(filename=""):
     '''Read a text file (UTF8) and prints it to stdout.'''
 
     with open(filename, 'r', encoding='UTF-8') as f:
-        print(f.read())
+        print(f.read(), end='')
