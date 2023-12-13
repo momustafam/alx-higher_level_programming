66e3d5b0553bea3fee07c82939c61d241ec25759
Mohammed Mustafa <mohammed112025@gmail.com>
2023-12-13 06:14:27 -0800

    Auto pushing

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,7 +1,8 @@
 /* A script that lists all shows contained in hbtn_0d_tvshows that have
 at least one genre linked. */
 -- task 11
-SELECT tv_shows.title, tv_show_genres.genre_id
-	FROM tv_shows
-	JOIN tv_show_genres ON tv_shows.id = tv_show_genres.genre_id
-	ORDER BY tv_shows.title, tv_show_genres.genre_id;
+SELECT s.title, g.genre_id
+	FROM tv_shows AS s
+		JOIN tv_show_genres AS g
+		ON s.id = g.show_id
+	ORDER BY s.title, g.show_id;
