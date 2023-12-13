fe5226398e47cb3e466e97890a339889b903e59c
Mohammed Mustafa <mohammed112025@gmail.com>
2023-12-13 04:02:54 -0800

    Auto pushing

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,6 +1,6 @@
 /* Creates the MySQL server user `user_0d_1`. with all privileges */
 -- Create the user
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost'
-       IDENTIFITED BY 'user_0d_1_pwd';
+       IDENTIFIED BY 'user_0d_1_pwd';
 -- Grant all privileges to the user
 GRANT ALL ON *.* TO 'user_0d_1'@'localhost';
