58310fa95075417b33bac215c932411b83271166
Mohammed Mustafa <mohammed112025@gmail.com>
2023-12-13 03:50:50 -0800

    Auto pushing

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,5 +1,5 @@
 /* lists all privileges of the MySQL users `user_0d_1` and `user_0d_2` on my server (in localhost). */
 -- first user
-SHOW GRANT FOR user_0d_1@localhost;
+SHOW GRANTS FOR user_0d_1@localhost;
 -- second user
-SHOW GRANT FOR user_0d_2@localhost;
+SHOW GRANTS FOR user_0d_2@localhost;
