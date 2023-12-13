082659ff4e074452b5cf224354475a830918335d
Mohammed Mustafa <mohammed112025@gmail.com>
2023-12-13 09:28:41 -0800

    Auto pushing

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -2,10 +2,10 @@
 of the show `Dexter` */
 -- task 15
 SELECT g.name
-	FROM tv_shows AS s
+	FROM tv_genres AS g
 		JOIN tv_show_genres AS sg
-		ON s.id = sg.show_id
-		JOIN tv_genres AS g
-		ON sg.genre_id = g.id
+		ON g.id = sg.genre_id
+		JOIN tv_shows AS s
+		ON sg.show_id = s.id
 	WHERE s.title = 'Dexter'
 	ORDER BY g.name;
