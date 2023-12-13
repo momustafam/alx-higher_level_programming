4a89d7cd66bec2d4899ef697989130779e8bb354
Mohammed Mustafa <mohammed112025@gmail.com>
2023-12-13 04:08:44 -0800

    Auto pushing

---
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,10 @@
+/* Creates the database hbtn_0d_2 and the user user_0d_2. */
+-- Create the database
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+
+-- Create the user
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost'
+       IDENTIFIED BY 'user_0d_2_pwd';
+
+-- Grant privileges to the user
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
