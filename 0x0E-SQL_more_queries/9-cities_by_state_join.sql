98fdc4f325a443a4377368cc251f10d27fa8d0f9
Mohammed Mustafa <mohammed112025@gmail.com>
2023-12-13 05:51:12 -0800

    Auto pushing

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,5 +1,6 @@
 /* A script that lists all cities contained in the database hbtn_0d_usa. */
 -- the query
-SELECT cities.id, cities.name, states.names
-	FROM cities, states
-	ORDER BY cities.id;
+SELECT cities.id, cities.name, states.name
+FROM cities
+JOIN states ON cities.state_id = states.id
+ORDER BY cities.id;
