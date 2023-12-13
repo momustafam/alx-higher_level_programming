215a016f8e56803c8d60bf4acb1cb4520d9ff3f6
Mohammed Mustafa <mohammed112025@gmail.com>
2023-12-13 09:30:54 -0800

    Auto pushing

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -1,10 +1,8 @@
-/* A script that uses the hbtn_0d_tvshows databases to lists all genres of the show `Dexter` */
+/* A script that uses the databases to lists all genres of the show `Dexter` */
 -- task 15
 SELECT g.name
-	FROM tv_genres AS g
-		JOIN tv_show_genres AS sg
-		ON g.id = sg.genre_id
-		JOIN tv_shows AS s
-		ON sg.show_id = s.id
-	WHERE s.title = 'Dexter'
-	ORDER BY g.name;
+FROM tv_genres AS g
+JOIN tv_show_genres AS sg ON g.id = sg.genre_id
+JOIN tv_shows AS s ON sg.show_id = s.id
+WHERE s.title = 'Dexter'
+ORDER BY g.name;
