83d8ed06873adf225ef8f2a90ee75f587a9f9ec3
Mohammed Mustafa <mohammed112025@gmail.com>
2023-12-13 06:55:03 -0800

    Auto pushing

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,8 +1,9 @@
 /* A script */
 -- task 14
-SELECT g.name AS genre, COUNT(*) AS number_of_shows
-	FROM tv_show_genres as g
-		JOIN tv_shows as s
-		ON g.genre_id = s.id
-	GROUP BY name
-	ORDER BY number_of_shows DESC;
+SELECT g.`name` AS `genre`,
+       COUNT(*) AS `number_of_shows`
+  FROM `tv_genres` AS g
+       INNER JOIN `tv_show_genres` AS t
+       ON g.`id` = t.`genre_id`
+ GROUP BY g.`name`
+ ORDER BY `number_of_shows` DESC;
