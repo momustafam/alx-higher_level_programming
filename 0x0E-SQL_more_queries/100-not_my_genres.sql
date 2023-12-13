4e0d5ccd469e4723f0e490eddea76ecfd969f7e8
Mohammed Mustafa <mohammed112025@gmail.com>
2023-12-13 09:32:09 -0800

    Auto pushing

--- 0x0E-SQL_more_queries/100-not_my_genres.sql
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -1,12 +1,9 @@
-/* A script that uses the hbtn_0d_tvshows databases to list all genres 
-not linked to the show `Dexter` */
--- task 18
-SELECT name
-	FROM tv_genres
-    	WHERE tv_genres.id NOT IN (
-		SELECT tv_genres.id FROM tv_genres
-		JOIN tv_show_genres ON tv_genres.id=tv_show_genres.genre_id
-		JOIN tv_shows ON tv_shows.id=tv_show_genres.show_id
-		WHERE tv_shows.title = "DEXTER" 
-	)
-   ORDER BY name;
+-- uses the hbtn_0d_tvshows database to list all genres not linked to the show Dexter
+SELECT name 
+FROM tv_genres
+WHERE tv_genres.id NOT IN (
+SELECT tv_genres.id FROM tv_genres
+JOIN tv_show_genres ON tv_genres.id=tv_show_genres.genre_id
+JOIN tv_shows ON tv_shows.id=tv_show_genres.show_id
+WHERE tv_shows.title = DEXTER )
+ORDER BY tv_genres.name;
--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -1,8 +1,7 @@
-/* A script that uses the databases to lists all genres of the show `Dexter` */
--- task 15
-SELECT g.name
-FROM tv_genres AS g
-JOIN tv_show_genres AS sg ON g.id = sg.genre_id
-JOIN tv_shows AS s ON sg.show_id = s.id
-WHERE s.title = 'Dexter'
-ORDER BY g.name;
+--  uses the hbtn_0d_tvshows database to lists all genres of the show Dexter
+SELECT name 
+FROM tv_genres
+JOIN tv_show_genres ON id=tv_show_genres.genre_id
+JOIN tv_shows ON tv_shows.id=tv_show_genres.show_id
+WHERE tv_shows.title = 'Dexter'
+ORDER BY name;
