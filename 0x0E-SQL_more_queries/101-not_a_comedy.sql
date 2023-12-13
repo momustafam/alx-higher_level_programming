0d8ad26b668276d3c5fdaf4e98f5d456fabec0ca
Mohammed Mustafa <mohammed112025@gmail.com>
2023-12-13 09:57:17 -0800

    Auto pushing

--- 0x0E-SQL_more_queries/101-not_a_comedy.sql
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -3,7 +3,7 @@
 SET @comedy_genre_id = (SELECT id FROM tv_genres WHERE name = 'Comedy' LIMIT 1);
 
 -- List shows without the genre Comedy
-SELECT tv_shows.title
+SELECT DISTINCT(tv_shows.title)
 FROM tv_shows
 LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
 WHERE tv_show_genres.genre_id IS NULL OR tv_show_genres.genre_id <> @comedy_genre_id
