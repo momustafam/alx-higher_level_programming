4008a0a1421c8201ba83207580c892a1a2aa74e8
Mohammed Mustafa <mohammed112025@gmail.com>
2023-12-13 06:20:28 -0800

    Auto pushing

---
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -0,0 +1,7 @@
+/* A script that lists all shows contained in the hbtn_0d_tvshows. */
+-- task 12
+SELECT s.title, g.genre_id
+	FROM tv_shows AS s
+		LEFT JOIN tv_show_genres AS g
+		ON s.id = g.show_id
+	ORDER BY s.title, g.genre_id
