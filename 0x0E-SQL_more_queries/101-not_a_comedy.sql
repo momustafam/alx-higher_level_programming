f179ef8419c581c45699d36ce75a9e1946822365
Mohammed Mustafa <mohammed112025@gmail.com>
2023-12-13 09:58:57 -0800

    Auto pushing

--- 0x0E-SQL_more_queries/101-not_a_comedy.sql
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -1,10 +1,9 @@
-/* A script that lists all shows without the genre `Comedy` */
--- Get the genre_id for Comedy
-SET @comedy_genre_id = (SELECT id FROM tv_genres WHERE name = 'Comedy' LIMIT 1);
-
--- List shows without the genre Comedy
-SELECT DISTINCT(tv_shows.title)
+-- lists all shows without the genre Comedy in the database hbtn_0d_tvshows.
+SELECT tv_shows.title
 FROM tv_shows
-LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
-WHERE tv_show_genres.genre_id IS NULL OR tv_show_genres.genre_id <> @comedy_genre_id
+WHERE tv_shows.id NOT IN (
+	SELECT tv_shows.id FROM tv_shows
+	JOIN tv_show_genres ON tv_shows.id=tv_show_genres.show_id
+	JOIN tv_genres ON tv_genres.id=tv_show_genres.genre_id
+	WHERE tv_genres.name = "Comedy" )
 ORDER BY tv_shows.title;
