15a67c136e556d3e3e8877a93d1bed51facbcac4
Mohammed Mustafa <mohammed112025@gmail.com>
2023-12-13 06:25:34 -0800

    Auto pushing

---
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -0,0 +1,7 @@
+/* A script that lists all shows contained in hbtn_0d_shows without a genre linked. */
+-- task 13
+SELECT s.title, g.genre_id
+	FROM tv_shows AS s
+	RIGHT JOIN tv_show_genre AS g
+	ON s.id = g.show_id
+	ORDER BY s.title, g.genre_id;
