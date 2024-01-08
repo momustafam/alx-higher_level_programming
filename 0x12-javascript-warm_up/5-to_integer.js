edbd687e2586875a0a33e1613fcfa1b5c115c04f
Mohammed Mustafa <mohammed112025@gmail.com>
2024-01-08 01:27:38 -0800

    auto pushing

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
-const argv = process.argv
-let num = parseInt(argv[2])
+const argv = process.argv;
+let num = parseInt(argv[2]);
 if (!isNaN(num)) {
-    console.log(`My number: ${num}`)
+    console.log(`My number: ${num}`);
 } else {
-    console.log("Not a Number")
+    console.log('Not a Number');
 }
