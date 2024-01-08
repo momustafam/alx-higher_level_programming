9dec11d48ef1546465d38dfb3e205f18ec9c6e42
Mohammed Mustafa <mohammed112025@gmail.com>
2024-01-08 02:47:10 -0800

    auto pushing

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+const argv = prcoess.argv;
+const arg1 = parseInt(argv[2]);
+function factorial(a) {
+  if (a === 0 || a === 1) {
+    return a;
+  } else if (isNaN(a)) {
+      return 1;
+  }
+  return a * factorial(a - 1);
+}
+console.log(factorial(arg1));
---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,5 @@
+#!/usr/bin/usr
+const argv = process.argv
+function factorial(a) {
+  
+}
