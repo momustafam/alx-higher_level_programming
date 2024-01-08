53ad405301250f1d308bc8a5091cba1d70ad733b
Mohammed Mustafa <mohammed112025@gmail.com>
2024-01-08 01:55:10 -0800

    auto pushing

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,8 +1,10 @@
 #!/usr/bin/node
 const a = parseInt(process.argv[2]);
 const b = parseInt(process.argv[3]);
-function add (a, b) {
+
+function add(a, b) {
   const c = a + b;
   console.log(c);
 }
+
 add(a, b);
