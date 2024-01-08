53f4c3d40aa2e84d54b1eecf1d5d17cc101a3e8f
Mohammed Mustafa <mohammed112025@gmail.com>
2024-01-08 02:57:27 -0800

    auto pushing

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,5 +1,8 @@
-#!/usr/bin/usr
-const argv = process.argv
-function factorial(a) {
-  
+#!/usr/bin/node
+if (!process.argv[2] || !process.argv[3]) {
+  console.log(0);
+} else {
+  const args = process.argv.slice(2);
+  const intArgs = args.sort((a, b) => a - b);
+  console.log(intArgs[intArgs.length - 2]);
 }
