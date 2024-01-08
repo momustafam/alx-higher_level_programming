b365e6c864f439c1f17915d0d5974f3614d5460d
Mohammed Mustafa <mohammed112025@gmail.com>
2024-01-08 01:38:38 -0800

    auto pushing

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
-const argv = process.argv
-let num = parseInt(argv[2])
+const argv = process.argv;
+const num = parseInt(argv[2])
 if (!isNaN(num)) {
-    for (let i = 0; i < num; i++) {
-        console.log("C is fun")
-    }
+  for (let i = 0; i < num; i++) {
+    console.log('C is fun');
+  }
 } else {
-    console.log("Missing number of occurrences")
+    console.log('Missing number of occurrences');
 }
