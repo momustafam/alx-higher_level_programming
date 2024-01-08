b7cc1cb68fc03a2d75af5f700243f01c65ecdf40
Mohammed Mustafa <mohammed112025@gmail.com>
2024-01-08 02:52:05 -0800

    auto pushing

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -2,11 +2,7 @@
 const argv = process.argv;
 const arg1 = parseInt(argv[2]);
 function factorial(a) {
-  if (a === 0 || a === 1) {
-    return a;
-  } else if (isNaN(a)) {
-      return 1;
-  }
-  return a * factorial(a - 1);
+  if (isNaN(a) || a < 2) return 1;
+  else return a * factorial(a - 1);
 }
 console.log(factorial(arg1));
