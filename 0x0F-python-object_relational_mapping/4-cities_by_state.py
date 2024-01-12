3ed2ecad26152ca0c5b57741a71bb6e6d622af17
Mohammed Mustafa <mohammed112025@gmail.com>
2024-01-12 11:41:40 -0800

    auto pushing

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+'''
+    A simple module since it has a one function.
+'''
+import MySQLdb
+from sys import argv
+
+
+def main():
+    '''
+        Lists all cities from the database hbtn_0e_4_usa.
+    '''
+
+    db = MySQLdb.connect(host="localhost", user=argv[1],
+                         passwd=argv[2], db=argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT * FROM cities ORDER BY id")
+
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+
+    cur.close()
+    db.close()
+
+
+if __name__ == "__main__":
+    main()
