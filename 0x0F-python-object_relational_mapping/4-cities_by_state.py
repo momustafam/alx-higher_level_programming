0ae0492292842bc514fe5e57a3f53c274794de9c
Mohammed Mustafa <mohammed112025@gmail.com>
2024-01-12 11:42:57 -0800

    auto pushing

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -12,7 +12,7 @@ def main():
     '''
 
     db = MySQLdb.connect(host="localhost", user=argv[1],
-                         passwd=argv[2], db=argv[3])
+                         passwd=argv[2], db=argv[3], port=3306)
     cur = db.cursor()
     cur.execute("SELECT * FROM cities ORDER BY id")
 
