25e9a4d10aece69ee17bc4c60788dc676f404c0c
Mohammed Mustafa <mohammed112025@gmail.com>
2024-01-13 06:16:53 -0800

    auto pushing

---
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+'''
+    A simple module since it has a one function add_state.
+'''
+
+from model_state import Base, State
+from sys import argv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+def add_state(user, pas, db):
+    '''
+        Adds the `State` object "Louisiana" to the database hbtn_0e_0_usa.
+    '''
+
+    engine = create_engine(
+            f"mysql+mysqldb://{user}:{pas}@localhost:3306/{db}"
+            )
+    Session = sessionmaker(engine)
+    louisiana = State(name="Louisiana")
+
+    with Session() as session:
+        session.add(louisiana)
+        session.commit()
+
+    print(louisiana.id)
+
+
+if __name__ == "__main__":
+    add_state(argv[1], argv[2], argv[3])
