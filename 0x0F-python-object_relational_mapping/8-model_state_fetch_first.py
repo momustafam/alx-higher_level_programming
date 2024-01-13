7c61ee6d46dacf144fe805991269746ad232bbb9
Mohammed Mustafa <mohammed112025@gmail.com>
2024-01-13 05:00:53 -0800

    auto pushing

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+'''
+     A simple module since it has a one function.
+'''
+from sys import argv
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+def first_state(username, password, db_name):
+    '''
+        Prints the first `State` object from the databse hbtn_0e_0_usa.
+    '''
+
+    engine = create_engine(
+        "mysql+mysqldb://{username}:{password}@localhost:3306/db_name"
+    )
+    Session = sessionmaker(bind=engine)
+
+    with Session() as session:
+        state = session.query(State).order_by(State.id).first()
+    if state:
+        print(f"{state.id}: {state.name}")
+    else:
+        print()
+
+
+if __name__ == "__main__":
+    first_state(argv[1], argv[2], argv[3])
