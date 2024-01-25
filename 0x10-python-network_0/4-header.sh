32a5ddcbf6ad2629a6110a64f5a3aaa67bd4d666
Mohammed Mustafa <mohammed112025@gmail.com>
2024-01-25 03:46:32 -0800

    auto pushing

---
+++ 0x10-python-network_0/4-header.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# Takes an URL and sends a `GET` request to the URL, and displays the response
+curl -sH "X-School-User-Id: 98" "$1"
---
+++ 0x10-python-network_0/5-post_params.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# Sends a `POST` request to the passed URL and display the body of response
+curl -sX POST -d "email=test@gmail.com&subject=I will always be here for PLD"
