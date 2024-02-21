45cff74b174be1ff3476ffb515fe7c080c084693
Mohammed Mustafa <mohammedmustafa112025@gmail.com>
2024-02-20 16:42:19 -0800

    auto

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+
+const rq = require('request');
+
+const url = process.argv[2];
+const nComp = {};
+let completed;
+let userId;
+
+rq.get(url, (_, __, body) => {
+  const todos = JSON.parse(body);
+  for (let i = 0; i < todos.length; i++) {
+    userId = todos[i].userId;
+    completed = todos[i].completed;
+    if (completed && userId in nComp) nComp[userId] += 1;
+    else if (completed && !(userId in nComp)) nComp[userId] = 1;
+    else if (!completed && !(userId in nComp)) nComp[userId] = 0;
+  }
+  console.log(nComp);
+});
